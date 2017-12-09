fb7e54f28c17e1e9322b452ea729de85ea48f56c
kirill <kirill-nikonorov@mail.ru>
2017-12-09 21:59:39 +0300

    the third  variant , fixed recursion , Main-class name , added a passibility of entering data

--- brokenPanel/src/main/java/ru/nikonorovcompany/instructor/Instructor.java
+++ brokenPanel/src/main/java/ru/nikonorovcompany/instructor/Instructor.java
@@ -6,11 +6,13 @@ import ru.nikonorovcompany.service.InstructionProviderCreator;
 public class Instructor {
     public static void main(String[] args) {
 
-        String[] inputData = "2555 5".split(" ");
+        if (args.length==0){
+            args = "2555 5".split(" ");
+        }
 
-        InstructionProvider provider = new InstructionProviderCreator(inputData).createInstructionProvider();
+        InstructionProvider provider = new InstructionProviderCreator(args).createInstructionProvider();
 
-        System.out.println("требуемая комбинация = " + inputData[0]);
+        System.out.println("требуемая комбинация = " + args[0]);
         System.out.println("ближайшая комбинация = " + provider.getTheClosestCombination());
         System.out.println("количество итераций к ней = " + provider.getTheCountOfIterationsToRequiredCombination());
     }
